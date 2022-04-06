480879cc061af9a83654025a1c8b38581a2a2959
bayulee <bayuleepaul@gmail.com>
2022-04-06 14:53:37 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,13 @@
+const results=[
+    {name:"Aya Bayuo", class:"form one", marks:9},
+    {name:"eric dery", class:"form one", marks:7},
+    {name:"ivy Bayuo", class:"form one", marks:8},
+    {name:"paul Bayulee", class:"form one", marks:10},
+    {name:"felicia dabuo", class:"form one", marks:5},
+    {name:"Ayako Bay", class:"form one", marks:7},
+    {name:"erica dary", class:"form one", marks:8},
+    {name:"ivy naah", class:"form one", marks:8},
+    {name:"paul smith", class:"form one", marks:5},
+    {name:"felix dakura", class:"form one", marks:9}
+]
+module.exports=results
